b3cc446d111d8b6d51492cf912ce97ce7bc1bcf5
siddht5 <dhar.75.ap@outlook.in>
2017-12-13 12:59:34 +0530

    Create geo_ip.js

---
+++ resource/geo_ip.js
@@ -0,0 +1,15 @@
+var json = function (_data)
+{
+ document.write("Ip : "+_data.ip+"<br>");
+  document.write("Country code : "+_data.country_code+"<br>");
+                 document.write("Country Name : "+_data.country_name+"<br>");
+                 document.write("Region code : "+_data.region_code+"<br>");
+                 document.write("Region Name : "+_data.region_name+"<br>");
+                 document.write("City : "+_data.city+"<br>");
+                 document.write("Zip Code : "+_data.zip_code+"<br>");
+                 document.write("Time Zone : "+_data.time_zone+"<br>");
+                 document.write("Latitude : "+_data.latitude+"<br>");
+                 document.write("Longitude : "+_data.longitude+"<br>");
+                 document.write("Metro code : "+_data.metro_code+"<br>");
+ //document.getElementById("p1").innerHTML = _data.IP;
+};
