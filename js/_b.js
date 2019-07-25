ab7dc3e52942a826b7b2723a0a81ea918422419b
siddht4 <dhar.75.ap@gmail.com>
2019-07-26 02:17:26 +0530

    Create _b.js

---
+++ js/_b.js
@@ -0,0 +1,12 @@
+function b(_data)
+{
+
+var x = document.createElement("IMG");
+    x.setAttribute("src", _data.location.country_flag);
+    x.setAttribute("width", "100");
+    x.setAttribute("width", "100");
+    x.setAttribute("alt", "border image");
+    document.body.appendChild(x);
+
+
+}
